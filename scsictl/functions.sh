d9374121f014cc8a6730e2f7ce3bdbc55f1d24e1
Ilari Korhonen <ilarik@kth.se>
2017-05-18 19:11:51 +0200

    preliminary scsictl program
    for easier management of the linux scsi subsystem, uses the sysfs interface

---
+++ scsictl/functions.sh
@@ -0,0 +1,21 @@
+function listdevices() {
+
+    # glob thru the srp hosts found in sysfs, identified as srp_host/hostNN where NN host#                                                                 
+    for srp_hostdir in /sys/class/srp_host/host[0-9]*/; do
+	host=`basename $srp_hostdir`
+        printf "SRP host $host\n"
+
+        # glob thru scsi targets in srp host dir host/device/targetX:Y:Z where X host#, Y bus#, Z target#                                                  
+        for targetdir in $srp_hostdir/device/target[0-9]*:[0-9]*:[0-9]*/; do
+            target=`basename $targetdir`
+            printf "SRP host $host target $target\n"
+
+            # glob thru scsi devices in target dir target/X:Y:Z:N, where X,Y,Z as before, N lun#                                                           
+            for devicedir in $targetdir/[0-9]*:[0-9]*:[0-9]:[0-9]*/; do
+                device=`basename $devicedir`
+                lun=`echo $device|awk -F: '{print $4}'`
+                printf "SRP host $host target $target lun $lun (device $device)\n"
+            done
+	    done
+    done
+}
---
+++ scsictl/scsictl.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+source `dirname $0`/functions.sh
+
+case "$1" in
+    list)
+        listdevices
+        ;;
+    
+    status)
+        dev_status
+	;;
+    
+    delete)
+        dev_delete
+        ;;
+    
+    rescan)
+	rescan_bus
+        ;;
+
+    *)
+        echo $"Usage: $0 {list|status|delete|rescan}"
+        exit 1
+esac
