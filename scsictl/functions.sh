1ab8540b313028d1964050fe2f736dbeb26d31de
Ilari Korhonen <ilarik@kth.se>
2017-05-21 21:28:33 +0200

    major cleanup: do globs properly

--- scsictl/functions.sh
+++ scsictl/functions.sh
@@ -61,9 +61,13 @@ function show_srp()
 function show_all_scsi_hosts()
 {
     # glob thru the scsi hosts found in sysfs, identified as /class/scsi_host/hostNN where NN host#
-    for hostpath in /sys/class/scsi_host/host[0-9]*/; do
-        local host=`basename $hostpath`
-        show_scsi_host $host $1
+    local hostpaths="/sys/class/scsi_host/host[0-9]*"
+
+    for hostpath in $hostpaths; do
+	if [ -d $hostpath ]; then
+            local host=`basename $hostpath`
+            show_scsi_host $host $1
+	fi
     done
 }
 
@@ -87,10 +91,10 @@ function show_scsi_host()
     local supported_mode=$(<$hostpath/supported_mode)
     local active_mode=$(<$hostpath/active_mode)
 
-    printf "Host Interface: $host [state: $state, name: $proc_name, modes: $active_mode/$supported_mode (active/supported)]\n"
+    printf "Host Interface: $host "
+    printf "[state: $state, name: $proc_name, modes: $active_mode/$supported_mode (active/supported)]\n"
 
     if [ "$mode" == "detail" ] || [ "$mode" == "topology" ]; then
-    
 	# more detailed info per host (still SCSI generic)
 	local host_busy=$(<$hostpath/host_busy)
 	local sg_tablesize=$(<$hostpath/sg_tablesize)
@@ -149,12 +153,14 @@ function show_srp_host()
     printf "\tSRP Transport Layer Retry count: $tl_retry_count\n"
 
     if [ "$mode" == "topology" ]; then
-	if [ -d $hostpath/device/target[0-9]*:[0-9]*:[0-9]*/ ]; then
-	    # glob thru scsi targets in srp host path host/device/targetX:Y:Z where X host#, Y bus#, Z target#
-	    for targetpath in $hostpath/device/target[0-9]*:[0-9]*:[0-9]*/; do
+	# glob thru scsi targets in srp host path host/device/targetX:Y:Z where X host#, Y bus#, Z target#
+	local targetpaths="$hostpath/device/target[0-9]*:[0-9]*:[0-9]*"
+
+	for targetpath in $targetpaths; do
+	    if [ -d $targetpath ]; then
 		show_scsi_target $targetpath "\t"
-	    done
-	fi
+	    fi
+	done
     fi
 }
 
@@ -174,8 +180,12 @@ function show_scsi_target()
     printf "\n$prepend""SCSI Target $target\n"
 
     # glob thru scsi devices in target path target/X:Y:Z:N, where X,Y,Z as before, N lun#
-    for devicepath in $targetpath/[0-9]*:[0-9]*:[0-9]*:[0-9]*/; do
-	show_scsi_lun $devicepath "$prepend\t"
+    local devicepaths="$targetpath/[0-9]*:[0-9]*:[0-9]*:[0-9]*"
+
+    for devicepath in $devicepaths; do
+	if [ -d $devicepath ]; then
+	    show_scsi_lun $devicepath "$prepend\t"
+	fi
     done
 }
 
@@ -260,96 +270,114 @@ function show_sas_node()
 
     local devname=`basename $devpath`
 
-    # if the device has SCSI targets
-    if [ -d $devpath/target[0-9]*:[0-9]*:[0-9]* ]; then
-        # glob thru SCSI targets found in this end device
-        for targetpath in $devpath/target[0-9]*:[0-9]*:[0-9]*; do
-            show_scsi_target $targetpath "$prepend"
-        done
-	printf "\n"
-    fi
-
     # check if we are a SAS expander
-    if [ -d $devpath/sas_expander/$devname ]; then
-	local product_id=$(<$devpath/sas_expander/$devname/product_id)
-	local product_rev=$(<$devpath/sas_expander/$devname/product_rev)
+    local expanderpath="$devpath/sas_expander/$devname"
+    
+    if [ -d $expanderpath ]; then
+	local product_id=$(<$expanderpath/product_id)
+	local product_rev=$(<$expanderpath/product_rev)
 	
 	printf "\n$prepend""SAS Expander $devname [product: '$product_id', revision: $product_rev]\n"
 	prepend="$prepend""\t"
     fi
-
+    
     # glob thru SAS PHY interfaces available
-    for phypath in $devpath/phy-*; do
-	local phy=`basename $phypath`
-	local phy_devpath="$devpath/$phy/sas_phy/$phy"
+    local phypaths="$devpath/phy-*"
 
-	local sas_address=$(<$phy_devpath/sas_address)
-	local maximum_linkrate=$(<$phy_devpath/maximum_linkrate)
-	local minimum_linkrate=$(<$phy_devpath/minimum_linkrate)
-	local negotiated_linkrate=$(<$phy_devpath/negotiated_linkrate)
-
-	printf "$prepend""PHY $phy at SAS Address $sas_address "
-	printf "[link rates: $minimum_linkrate/$maximum_linkrate (min/max), negotiation status: $negotiated_linkrate]\n"
+    for phypath in $phypaths; do
+	if [ -d $phypath ]; then
+	    local phy=`basename $phypath`
+	    local phy_devpath="$devpath/$phy/sas_phy/$phy"
+	
+	    local sas_address=$(<$phy_devpath/sas_address)
+	    local maximum_linkrate=$(<$phy_devpath/maximum_linkrate)
+	    local minimum_linkrate=$(<$phy_devpath/minimum_linkrate)
+	    local negotiated_linkrate=$(<$phy_devpath/negotiated_linkrate)
+	    
+	    printf "$prepend""PHY $phy at SAS Address $sas_address "
+	    printf "[link rates: $minimum_linkrate/$maximum_linkrate (min/max), negotiation status: $negotiated_linkrate]\n"
+	fi
     done
 
     # glob thru SAS active ports
-     for portpath in $devpath/port-*; do
-	 local port=`basename $portpath`
-	 local port_devpath="$devpath/$port/sas_port/$port"
+    local portpaths="$devpath/port-*"
+    
+    for portpath in $portpaths; do
+	if [ -d $portpath ]; then
+	    local port=`basename $portpath`
+	    local port_devpath="$devpath/$port/sas_port/$port"
+	    
+	    local num_phys=$(<$port_devpath/num_phys)
+	    
+	    printf "\n$prepend""Port $port [PHY count: $num_phys]\n"
+	    printf "$prepend\t""Assigned PHY(s):"
+	    
+	    # glob thru SAS PHYs attached to this port
+	    local phypaths="$portpath/phy-*"
+	    
+	    for phypath in $phypaths; do
+		if [ -d $phypath ]; then
+		    local phy=`basename $phypath`
+		    printf " $phy"
+		fi
+	    done
+	    printf "\n"
+	    
+	    # glob thru end devices at this node, if present
+	    local enddevpaths="$portpath/end_device-*"
+	    
+	    for enddevpath in $enddevpaths; do
+		if [ -d $enddevpath ]; then
+		    local enddev=`basename $enddevpath`
+		    local enddev_devpath="$enddevpath/sas_device/$enddev"
+		    
+		    local sas_address=$(<$enddev_devpath/sas_address)
+		    local device_type=$(<$enddev_devpath/device_type)
+		    local enclosure_identifier=$(<$enddev_devpath/enclosure_identifier)
+		    local bay_identifier=$(<$enddev_devpath/bay_identifier)
+		    local initiator_port_protocols=$(<$enddev_devpath/initiator_port_protocols)
+		    local target_port_protocols=$(<$enddev_devpath/target_port_protocols)
+		    
+		    printf "\n"
+		    printf "$prepend\t""Device $enddev at SAS Address $sas_address\n"
+		    printf "$prepend\t\t""Device Type: $device_type\n"
+		    printf "$prepend\t\t""Initiator Port Protocols: $initiator_port_protocols\n"
+		    printf "$prepend\t\t""Target Port Protocols: $target_port_protocols\n"
+		    
+		    [ "$enclosure_identifier" != "" ] && printf "$prepend\t\t""Enclosure ID: $enclosure_identifier\n"
+		    [ "$bay_identifier" != "" ] && printf "$prepend\t\t""Bay ID: $bay_identifier\n"
+		    
+		    # glob thru SCSI targets found in this end device
+		    local enddev_targets="$enddevpath/target[0-9]*:[0-9]*:[0-9]*"
+		    
+		    for enddev_target in $enddev_targets; do
+			if [ -d $enddev_target ]; then
+			    show_scsi_target $enddev_target "$prepend\t\t"
+			fi
+		    done
+		fi
+	    done
+	    
+	    # glob thru the SAS expanders in this port
+	    local sasexpanders="$portpath/expander-*"
+	    
+	    for expanderpath in $sasexpanders; do
+		if [ -d $expanderpath ]; then
+		    show_sas_node $expanderpath "$prepend\t\t"
+		fi
+	    done
+	fi
+    done
 
-	 local num_phys=$(<$port_devpath/num_phys)
-
-	 printf "\n$prepend""Port $port [PHY count: $num_phys]\n"
-	 printf "$prepend\t""Assigned PHY(s):"
-
-	 # glob thru SAS PHYs attached to this port
-	 for phypath in $portpath/phy-*; do 
-	     local phy=`basename $phypath`
-	     printf " $phy"
-	 done
-	 printf "\n"
-	
-	 # if we have end devices at this node
-	 if [ -d $portpath/end_device-* ]; then
-	     # glob thru SAS end devices found from this port
-	     for enddevpath in $portpath/end_device-*; do 
-		 local enddev=`basename $enddevpath`
-		 local enddev_devpath="$enddevpath/sas_device/$enddev"
-
-		 local sas_address=$(<$enddev_devpath/sas_address)
-		 local device_type=$(<$enddev_devpath/device_type)
-		 local enclosure_identifier=$(<$enddev_devpath/enclosure_identifier)
-		 local bay_identifier=$(<$enddev_devpath/bay_identifier)
-		 local initiator_port_protocols=$(<$enddev_devpath/initiator_port_protocols)
-		 local target_port_protocols=$(<$enddev_devpath/target_port_protocols)
-
-		 printf "\n"
-		 printf "$prepend\t""Device $enddev at SAS Address $sas_address\n"
-		 printf "$prepend\t\t""Device Type: $device_type\n"
-		 printf "$prepend\t\t""Initiator Port Protocols: $initiator_port_protocols\n"
-		 printf "$prepend\t\t""Target Port Protocols: $target_port_protocols\n"
-
-		 [ "$enclosure_identifier" != "" ] && printf "$prepend\t\t""Enclosure ID: $enclosure_identifier\n"
-		 [ "$bay_identifier" != "" ] && printf "$prepend\t\t""Bay ID: $bay_identifier\n"
-		 
-		 # if the device has SCSI targets
-		 if [ -d $enddevpath/target[0-9]*:[0-9]*:[0-9]* ]; then
-		     # glob thru SCSI targets found in this end device
-		     for targetpath in $enddevpath/target[0-9]*:[0-9]*:[0-9]*; do 
-			 show_scsi_target $targetpath "$prepend\t\t"
-		     done
-		 fi
-	     done
-	 fi
-
-	 # if we have SAS expanders, recurse into them
-	 if [ -d $portpath/expander-* ]; then
-	     # glob thru the SAS expanders in this port
-	     for expanderpath in $portpath/expander-*; do
-		 show_sas_node $expanderpath "$prepend\t\t"
-	     done
-	 fi
-     done
+    # glob thru SCSI targets found in this node
+    local targetpaths="$devpath/target[0-9]*:[0-9]*:[0-9]*"
+    
+    for targetpath in $targetpaths; do
+	if [ -d $targetpath ]; then
+	    show_scsi_target $targetpath "$prepend"
+	    printf "\n"
+	fi
+    done
 }
 
 
@@ -361,42 +389,78 @@ function show_fc_host()
 
 function show_all_srp_nodes()
 {
-    # glob thru all the SRP subsystems found
-    for srpnodepath in /sys/class/infiniband_srp/*; do
-	show_srp_node $srpnodepath $1
+    # glob thru all the SRP paths 
+    local srpnodes="/sys/class/infiniband_srp/*"
+
+    for srpnodepath in $srpnodes; do
+	if [ -d $srpnodepath ]; then
+	    show_srp_node $srpnodepath $1
+	fi
     done
 }
+
 
 function show_srp_node()
 {
     local srpnodepath=$1
     local mode=$2
 
+    # SRP node name from the sysfs path
     local srpnode=`basename $srpnodepath`
 
+    # get associated InfiniBand HCA and port
     local ibdev=$(<$srpnodepath/ibdev)
     local port=$(<$srpnodepath/port)
 
     printf "InfiniBand SRP Node: $srpnode [device $ibdev, port $port]\n"
 
     if [ "$mode" == "detail" ]; then
-	if [ -d $srpnodepath/device/infiniband/* ]; then
-	    fw_ver=$(<$srpnodepath/device/infiniband/*/fw_ver)
-	    hca_type=$(<$srpnodepath/device/infiniband/*/hca_type)
-	    node_desc=$(<$srpnodepath/device/infiniband/*/node_desc)
-	    hw_rev=$(<$srpnodepath/device/infiniband/*/hw_rev)
-	    node_type=$(<$srpnodepath/device/infiniband/*/node_type)
-	    node_guid=$(<$srpnodepath/device/infiniband/*/node_guid)
-	    sys_image_guid=$(<$srpnodepath/device/infiniband/*/sys_image_guid)
+	# construct sysfs path to InfiniBand HCA device
+	local ibdevpath=$srpnodepath/device/infiniband/$ibdev
+
+	# if we can access the HCA device, query some attributes
+	if [ -d $ibdevpath ]; then
+	    local fw_ver=$(<$ibdevpath/fw_ver)
+	    local hca_type=$(<$ibdevpath/hca_type)
+	    local node_desc=$(<$ibdevpath/node_desc)
+	    local hw_rev=$(<$ibdevpath/hw_rev)
+	    local node_type=$(<$ibdevpath/node_type)
+	    local node_guid=$(<$ibdevpath/node_guid)
+	    local sys_image_guid=$(<$ibdevpath/sys_image_guid)
 
 	    printf "\tHCA Type: $hca_type [hw revision: $hw_rev, firmware: $fw_ver]\n"
 	    printf "\tNode Type: '$node_type' [description: '$node_desc']\n"
 	    printf "\tNode GUID: $node_guid\n"
 	    printf "\tSystem Image GUID: $sys_image_guid\n"
+	else
+	    printf "ERROR: InfiniBand HCA Device $ibdev not found in sysfs path $ibdevpath\n"
 	fi
 
-	numa_node=$(<$srpnodepath/device/numa_node)
+	# get NUMA node# for the PCI device
+	local numa_node=$(<$srpnodepath/device/numa_node)
 	printf "\tNUMA Node: $numa_node\n"
 	printf "\n"
+
+	# get SCSI host interfaces for this SRP subsystem
+	local srphosts=$(show_srp_node_hosts $ibdev $port)
+	printf "\tSRP SCSI Hosts: $srphosts\n"
     fi
+}
+
+
+function show_srp_node_hosts()
+{
+    local ibdev=$1
+    local port=$2
+
+    # define glob for srp host paths in sysfs
+    local srphosts="/sys/class/srp_host/host[0-9]*"
+
+    # glob thru SRP hosts
+    for srphostpath in $srphosts; do
+	if [ -d $srphostpath ]; then
+	    srphost=`basename $srphostpath`
+	    echo "found SRP SCSI host $srphost"
+	fi
+    done
 }
