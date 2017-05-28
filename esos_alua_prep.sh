d0f2f796507d72d57b475c2a2653cbc82cfbba64
Ilari Korhonen <ilarik@kth.se>
2017-05-29 00:13:15 +0200

    reindent

--- esos_alua_prep.sh
+++ esos_alua_prep.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 println() {
-          printf "\e[1m$1\e[0m\n"
+    printf "\e[1m$1\e[0m\n"
 }
 
 
@@ -26,24 +26,24 @@ scstadmin -add_tgrp local -dev_group $ALUA_DEVGROUP
 scstadmin -set_tgrp_attr local -dev_group $ALUA_DEVGROUP -attributes group_id=1
 
 for target in $ALUA_TARGETS; do
-	println "Adding SRP target to ALUA device group $ALUA_DEVGROUP target group local: $target"
-
-	scstadmin -add_tgrp_tgt $target -dev_group $ALUA_DEVGROUP -tgt_group local
-	scstadmin -set_ttgt_attr $target -driver ib_srpt -dev_group $ALUA_DEVGROUP -tgt_group local -attributes rel_tgt_id=$rel_tgt_id
-
-	((rel_tgt_id++))
+    println "Adding SRP target to ALUA device group $ALUA_DEVGROUP target group local: $target"
+    
+    scstadmin -add_tgrp_tgt $target -dev_group $ALUA_DEVGROUP -tgt_group local
+    scstadmin -set_ttgt_attr $target -driver ib_srpt -dev_group $ALUA_DEVGROUP -tgt_group local -attributes rel_tgt_id=$rel_tgt_id
+    
+    ((rel_tgt_id++))
 done
 
 for target in $ALUA_TARGETS; do
-	for host_group in $HOST_GROUPS; do
-		println "Creating host group for target $target: $host_group"
-		scstadmin -add_group $host_group -driver ib_srpt -target $target
-	done
+    for host_group in $HOST_GROUPS; do
+	println "Creating host group for target $target: $host_group"
+	scstadmin -add_group $host_group -driver ib_srpt -target $target
+    done
 done
 
 for target in $ALUA_TARGETS; do
-	println "Enabling SRP target: $target"	
-	scstadmin -enable_target $target -driver ib_srpt
+    println "Enabling SRP target: $target"	
+    scstadmin -enable_target $target -driver ib_srpt
 done
 
 println "Synchronizing ESOS configuration..."
--- map_sas_devices.sh
+++ map_sas_devices.sh
@@ -16,74 +16,74 @@ scst_lun=0
 hostpaths="/sys/class/sas_host/host"
 
 for i in {0..1023}; do
-	hostpath=$hostpaths$i
-
-	if [ ! -d $hostpath ]; then
-		continue;
+    hostpath=$hostpaths$i
+    
+    if [ ! -d $hostpath ]; then
+	continue;
+    fi
+    
+    host=`basename $hostpath`
+    printf "Found SAS host at SCSI host $i ($host)\n"
+    
+    portpaths="$hostpath/device/port-$i:"
+    
+    for j in {0..127}; do
+	portpath=$portpaths$j
+	
+	if [ ! -d $portpath ]; then
+	    break;
 	fi
- 
-	host=`basename $hostpath`
-	printf "Found SAS host at SCSI host $i ($host)\n"
 
-	portpaths="$hostpath/device/port-$i:"
-
-	for j in {0..127}; do
-		portpath=$portpaths$j
-
-		if [ ! -d $portpath ]; then
-			break;
-		fi
-
-		port=`basename $portpath`
-		end_devpath="$portpath/end_device-$i:$j"
-
-		if [ ! -d $end_devpath ]; then
-			break;
-		fi
-
-		end_dev=`basename $end_devpath`
-		targetpaths="$end_devpath/target$i:[0-9]:$j"
-
-		for targetpath in $targetpaths; do
-			if [ -d $targetpath ]; then
-				target=`basename $targetpath`
-				lunpaths="$targetpath/$i:[0-9]:$j:[0-9]"
-
-				for lunpath in $lunpaths; do
-					if [ -d $lunpath ]; then
-						lun=`basename $lunpath`
-						printf "\tFound SAS LUN $lun at SAS host $host port $port end device $end_dev target $target\n"
-
-						devpath="$lunpath/block/sd[a-z]*"
-						if [ -d $devpath ]; then
-							dev=`basename $devpath`
-
-							dev_id=`/usr/lib/udev/scsi_id -g /dev/$dev`
-							dev_name="scsi-$dev_id"
-							dev_by_id="/dev/disk/by-id/$dev_name"
-
-							if [ -e $dev_by_id ]; then
-								printf "\t\tFound attached block device $dev with SCSI ID $dev_id [persistent path: $dev_by_id]\n"
-								printf "\t\tMapping device $dev_by_id to SCST device $dev_name\n"
-
-								scstadmin -open_dev $dev_name -handler vdisk_blockio -attributes filename=$dev_by_id
-								scstadmin -set_dev_attr $dev_name -attributes threads_pool_type=per_initiator,threads_num=$NUM_THREADS
-								scstadmin -add_dgrp_dev $dev_name -dev_group $ALUA_DEVGROUP
-
-								for srp_target in $TARGETS; do
-									for host_group in $HOST_GROUPS; do 
-										printf "\t\t\tMapping SCST device $dev_name to LUN $scst_lun at SRP target $srp_target host group $host_group\n"
-
-										scstadmin -add_lun $scst_lun -driver ib_srpt -target $srp_target -group $host_group -device $dev_name
-									done	
-								done
-
-								((scst_lun++))
-							fi
-						fi
-					fi
+	port=`basename $portpath`
+	end_devpath="$portpath/end_device-$i:$j"
+	
+	if [ ! -d $end_devpath ]; then
+	    break;
+	fi
+	
+	end_dev=`basename $end_devpath`
+	targetpaths="$end_devpath/target$i:[0-9]:$j"
+	
+	for targetpath in $targetpaths; do
+	    if [ -d $targetpath ]; then
+		target=`basename $targetpath`
+		lunpaths="$targetpath/$i:[0-9]:$j:[0-9]"
+		
+		for lunpath in $lunpaths; do
+		    if [ -d $lunpath ]; then
+			lun=`basename $lunpath`
+			printf "\tFound SAS LUN $lun at SAS host $host port $port end device $end_dev target $target\n"
+			
+			devpath="$lunpath/block/sd[a-z]*"
+			if [ -d $devpath ]; then
+			    dev=`basename $devpath`
+			    
+			    dev_id=`/usr/lib/udev/scsi_id -g /dev/$dev`
+			    dev_name="scsi-$dev_id"
+			    dev_by_id="/dev/disk/by-id/$dev_name"
+			    
+			    if [ -e $dev_by_id ]; then
+				printf "\t\tFound attached block device $dev with SCSI ID $dev_id [persistent path: $dev_by_id]\n"
+				printf "\t\tMapping device $dev_by_id to SCST device $dev_name\n"
+				
+				scstadmin -open_dev $dev_name -handler vdisk_blockio -attributes filename=$dev_by_id
+				scstadmin -set_dev_attr $dev_name -attributes threads_pool_type=per_initiator,threads_num=$NUM_THREADS
+				scstadmin -add_dgrp_dev $dev_name -dev_group $ALUA_DEVGROUP
+				
+				for srp_target in $TARGETS; do
+				    for host_group in $HOST_GROUPS; do 
+					printf "\t\t\tMapping SCST device $dev_name to LUN $scst_lun at SRP target $srp_target host group $host_group\n"
+					
+					scstadmin -add_lun $scst_lun -driver ib_srpt -target $srp_target -group $host_group -device $dev_name
+				    done	
 				done
+				
+				((scst_lun++))
+			    fi
 			fi
+		    fi
 		done
+	    fi
 	done
+    done
 done
